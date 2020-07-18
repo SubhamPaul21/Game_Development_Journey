3328951248a01152025887118186b13f9a05f2c5
SubhamPaul21 <subhampaul12@gmail.com>
2020-07-18 21:34:07 +0530

    Add rocket controller script

---
+++ Game_Dev_Projects/Crazy Rocket/Assets/Scripts/RocketController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    private Rigidbody rocketRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketRigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
+    }
+}
