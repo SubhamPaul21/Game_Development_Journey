c9084ac84bac2d62bdafa044c51e5d961a440cdd
SubhamPaul21 <subhampaul12@gmail.com>
2020-07-19 21:20:10 +0530

    modified controller script

--- Game_Dev_Projects/Crazy Rocket/Assets/Scripts/RocketController.cs
+++ Game_Dev_Projects/Crazy Rocket/Assets/Scripts/RocketController.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RocketController : MonoBehaviour
 {
+    // member variables
     private Rigidbody rocketRigidBody;
     private AudioSource audioSource;
+
+    // state variables
+    [SerializeField] private float cylinderSpeed = 100f;
+    [SerializeField] private float thrustSpeed = 1000f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,31 +20,39 @@ public class RocketController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerInput();
+        Thrust();
+        Rotate();
     }
 
-    private void PlayerInput()
+    private void Thrust()
     {
+        float thrustThisFrame = thrustSpeed * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
-            rocketRigidBody.AddRelativeForce(Vector3.up);
+            rocketRigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
         }
         else
         {
             audioSource.Stop();
         }
+    }
 
+    private void Rotate()
+    {
+        float cylinderSpeedThisFrame = cylinderSpeed * Time.deltaTime;
+        rocketRigidBody.freezeRotation = true; //take manual control of rotation
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime);
+            transform.Rotate(Vector3.forward * cylinderSpeedThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back * Time.deltaTime);
+            transform.Rotate(Vector3.back * cylinderSpeedThisFrame);
         }
+        rocketRigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
