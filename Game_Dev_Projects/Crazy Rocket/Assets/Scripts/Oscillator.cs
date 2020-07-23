ca37896f42b4489a6325837754677b098ca814d7
SubhamPaul21 <subhampaul12@gmail.com>
2020-07-24 02:00:04 +0530

    Added lights

--- Game_Dev_Projects/Crazy Rocket/Assets/Scripts/Oscillator.cs
+++ Game_Dev_Projects/Crazy Rocket/Assets/Scripts/Oscillator.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     // state variables
-    [SerializeField] Vector3 movementVector;
-    [Range(0, 1)] [SerializeField] float movementFactor;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 5f;
 
     //member variables
     Vector3 startingPos;
@@ -22,6 +22,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f; // Approx to 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // Sin restricts between -1 to +1
+        float movementFactor = rawSinWave / 2f + 0.5f; // Divide by 2 to clamp b/w -0.5 to +0.5, then add 0.5 to clamp b/w 0 to 1
+        print(movementFactor);
         Vector3 offsetPos = movementVector * movementFactor;
         transform.position = startingPos + offsetPos;
     }
