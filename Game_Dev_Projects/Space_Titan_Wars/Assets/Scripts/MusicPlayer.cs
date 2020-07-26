2e278af99e14b64cc141d1ebec78e23a034c2552
SubhamPaul21 <subhampaul12@gmail.com>
2020-07-27 02:01:24 +0530

    Add player and wayPoint

---
+++ Game_Dev_Projects/Space_Titan_Wars/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer Instance { get; private set; }
+
+    private void Awake()
+    {
+        SceneManagerSingleton();
+    }
+
+    void SceneManagerSingleton()
+    {
+        if (GameObject.FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadGame", 8f);
+    }
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Game_Dev_Projects/Space_Titan_Wars/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(horizontal);
+    }
+}
