8e56cc6c451867f24d6cf3f68060c868e8a17aa4
SubhamPaul21 <subhampaul12@gmail.com>
2020-07-28 01:48:22 +0530

    Added player movement

--- Game_Dev_Projects/Space_Titan_Wars/Assets/Scripts/Player.cs
+++ Game_Dev_Projects/Space_Titan_Wars/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontal);
+        MovementInput();
     }
+
+    private void MovementInput()
+    {
+        // X Position
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetValue = speed * horizontalThrow * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffsetValue;
+        float clampedXPos = Mathf.Clamp(rawXPos, -12f, 12f);
+
+        // Y Position
+        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetValue = speed * verticalThrow * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffsetValue;
+        float clampedYPos = Mathf.Clamp(rawYPos, -6f, 6f);
+
+        // Set the local position
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
 }
