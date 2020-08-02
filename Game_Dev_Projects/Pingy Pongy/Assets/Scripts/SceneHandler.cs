8e99496bed985bea7bf5c28b1c4eac7d3bae497f
SubhamPaul21 <subhampaul12@gmail.com>
2020-08-02 16:52:54 +0530

    Added Menu Scene

---
+++ Game_Dev_Projects/Pingy Pongy/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public static SceneHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (GameObject.FindObjectsOfType<SceneHandler>().Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Load Game Scene
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Load Menu Scene
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Quit Application
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
