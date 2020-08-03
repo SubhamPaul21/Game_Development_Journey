4e647d558d26884b16aa9e1ca657ad19d946c41e
SubhamPaul21 <subhampaul12@gmail.com>
2020-08-04 02:01:04 +0530

    Added Game scene mechanics

---
+++ Game_Dev_Projects/Pingy Pongy/Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] float force = 3f;
+    Rigidbody2D ballRigidBody;
+    Vector2[] ballPosArray = { Vector2.up, Vector2.down };
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidBody = GetComponent<Rigidbody2D>();
+        Vector2 randomBallPos = ballPosArray[Random.Range(0,2)];
+        ballRigidBody.AddForce(randomBallPos * force, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Vector2 reverseBallPos = collision.gameObject.name == "Player 1" ? Vector2.up : Vector2.down;
+        float randomForce = Random.Range(2, 4);
+        ballRigidBody.AddForce(reverseBallPos * randomForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Game_Dev_Projects/Pingy Pongy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 30f;
+    [SerializeField] float xClampThreshold = 2f;
+
+    Rigidbody2D playerRigidBody;
+    float dirX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessPlayerTranslation();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessPlayerVelocity();
+    }
+
+    private void ProcessPlayerTranslation()
+    {
+        // Movement for player
+        dirX = Input.acceleration.x * playerSpeed;
+        float xClampPos = Mathf.Clamp(transform.position.x, -xClampThreshold, xClampThreshold);
+        transform.position = new Vector2(xClampPos, transform.position.y);
+    }
+
+    private void ProcessPlayerVelocity()
+    {
+        playerRigidBody.velocity = new Vector2(dirX, 0f);
+    }
+}
