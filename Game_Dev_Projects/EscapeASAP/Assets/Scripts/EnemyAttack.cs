cb105fef1d6e797f80a3670894feb2ea9b69649d
SubhamPaul21 <subhampaul12@gmail.com>
2020-08-18 01:42:00 +0530

    Add player health and enemy attack function

---
+++ Game_Dev_Projects/EscapeASAP/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] int attackDamage = 30;
+    PlayerHealth playerHealth;
+
+    void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (playerHealth == null) { return; }
+        playerHealth.DamagePlayer(attackDamage);
+    }
+}
---
+++ Game_Dev_Projects/EscapeASAP/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 100;
+    
+    public void DamagePlayer(int damageAmount)
+    {
+        playerHealth -= damageAmount;
+        if (playerHealth <= 0)
+        {
+            print("you are dead man!!!!");
+        }
+    } 
+}
